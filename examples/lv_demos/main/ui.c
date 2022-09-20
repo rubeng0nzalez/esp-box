9089844d711803a56555de8561378acd5ce72960
rubeng0nzalez <rgonzalez.mywork@gmail.com>
2022-09-20 15:24:51 -0400

    Estable version integrated SquareLine and ESPBox project
    This version runs without any issues. Contains the UI test integrated with the BLE/ESPBOX and SquareLine projects

--- examples/lv_demos/main/ui.c
+++ examples/lv_demos/main/ui.c
@@ -1,7 +1,7 @@
 // SquareLine LVGL GENERATED FILE
 // EDITOR VERSION: SquareLine Studio 1.0.5
 // LVGL VERSION: 8.2
-// PROJECT: SquareLine_Project2
+// PROJECT: SquareLine_Project
 
 #include "ui.h"
 #include "ui_helpers.h"
@@ -11,13 +11,24 @@ lv_obj_t * ui_Screen1;
 lv_obj_t * ui_Label1;
 lv_obj_t * ui_Label2;
 lv_obj_t * ui_trailer;
+lv_obj_t * ui_Image12;
+lv_obj_t * ui_Image13;
+lv_obj_t * ui_Image1;
+lv_obj_t * ui_Label3;
+lv_obj_t * ui_Label4;
+lv_obj_t * ui_Image2;
+lv_obj_t * ui_Image3;
+lv_obj_t * ui_Image4;
+lv_obj_t * ui_Image5;
+lv_obj_t * ui_Label5;
+lv_obj_t * ui_Label6;
 
 ///////////////////// TEST LVGL SETTINGS ////////////////////
-#if LV_COLOR_DEPTH != 32
-    #error "LV_COLOR_DEPTH should be 32bit to match SquareLine Studio's settings"
+#if LV_COLOR_DEPTH != 16
+    #error "LV_COLOR_DEPTH should be 16bit to match SquareLine Studio's settings"
 #endif
-#if LV_COLOR_16_SWAP !=0
-    #error "#error LV_COLOR_16_SWAP should be 0 to match SquareLine Studio's settings"
+#if LV_COLOR_16_SWAP !=1
+    #error "#error LV_COLOR_16_SWAP should be 1 to match SquareLine Studio's settings"
 #endif
 
 ///////////////////// ANIMATIONS ////////////////////
@@ -77,6 +88,174 @@ void ui_Screen1_screen_init(void)
 
     lv_obj_add_flag(ui_trailer, LV_OBJ_FLAG_ADV_HITTEST);
     lv_obj_clear_flag(ui_trailer, LV_OBJ_FLAG_SCROLLABLE);
+
+    // ui_Image12
+
+    ui_Image12 = lv_img_create(ui_Screen1);
+    lv_img_set_src(ui_Image12, &ui_img_rsz_dexter_big_png);
+
+    lv_obj_set_width(ui_Image12, LV_SIZE_CONTENT);
+    lv_obj_set_height(ui_Image12, LV_SIZE_CONTENT);
+
+    lv_obj_set_x(ui_Image12, -132);
+    lv_obj_set_y(ui_Image12, -99);
+
+    lv_obj_set_align(ui_Image12, LV_ALIGN_CENTER);
+
+    lv_obj_add_flag(ui_Image12, LV_OBJ_FLAG_ADV_HITTEST);
+    lv_obj_clear_flag(ui_Image12, LV_OBJ_FLAG_SCROLLABLE);
+
+    // ui_Image13
+
+    ui_Image13 = lv_img_create(ui_Screen1);
+    lv_img_set_src(ui_Image13, &ui_img_rsz_tpmsicon_png);
+
+    lv_obj_set_width(ui_Image13, LV_SIZE_CONTENT);
+    lv_obj_set_height(ui_Image13, LV_SIZE_CONTENT);
+
+    lv_obj_set_x(ui_Image13, -70);
+    lv_obj_set_y(ui_Image13, -102);
+
+    lv_obj_set_align(ui_Image13, LV_ALIGN_CENTER);
+
+    lv_obj_add_flag(ui_Image13, LV_OBJ_FLAG_ADV_HITTEST);
+    lv_obj_clear_flag(ui_Image13, LV_OBJ_FLAG_SCROLLABLE);
+
+    // ui_Image1
+
+    ui_Image1 = lv_img_create(ui_Screen1);
+    lv_img_set_src(ui_Image1, &ui_img_rsz_tpmsicon_png);
+
+    lv_obj_set_width(ui_Image1, LV_SIZE_CONTENT);
+    lv_obj_set_height(ui_Image1, LV_SIZE_CONTENT);
+
+    lv_obj_set_x(ui_Image1, 70);
+    lv_obj_set_y(ui_Image1, -102);
+
+    lv_obj_set_align(ui_Image1, LV_ALIGN_CENTER);
+
+    lv_obj_add_flag(ui_Image1, LV_OBJ_FLAG_ADV_HITTEST);
+    lv_obj_clear_flag(ui_Image1, LV_OBJ_FLAG_SCROLLABLE);
+
+    // ui_Label3
+
+    ui_Label3 = lv_label_create(ui_Screen1);
+
+    lv_obj_set_width(ui_Label3, LV_SIZE_CONTENT);
+    lv_obj_set_height(ui_Label3, LV_SIZE_CONTENT);
+
+    lv_obj_set_x(ui_Label3, -70);
+    lv_obj_set_y(ui_Label3, -5);
+
+    lv_obj_set_align(ui_Label3, LV_ALIGN_CENTER);
+
+    lv_label_set_text(ui_Label3, "TPMS3");
+
+    // ui_Label4
+
+    ui_Label4 = lv_label_create(ui_Screen1);
+
+    lv_obj_set_width(ui_Label4, LV_SIZE_CONTENT);
+    lv_obj_set_height(ui_Label4, LV_SIZE_CONTENT);
+
+    lv_obj_set_x(ui_Label4, 70);
+    lv_obj_set_y(ui_Label4, -5);
+
+    lv_obj_set_align(ui_Label4, LV_ALIGN_CENTER);
+
+    lv_label_set_text(ui_Label4, "TPMS4");
+
+    // ui_Image2
+
+    ui_Image2 = lv_img_create(ui_Screen1);
+    lv_img_set_src(ui_Image2, &ui_img_rsz_tpmsicon_png);
+
+    lv_obj_set_width(ui_Image2, LV_SIZE_CONTENT);
+    lv_obj_set_height(ui_Image2, LV_SIZE_CONTENT);
+
+    lv_obj_set_x(ui_Image2, 70);
+    lv_obj_set_y(ui_Image2, 21);
+
+    lv_obj_set_align(ui_Image2, LV_ALIGN_CENTER);
+
+    lv_obj_add_flag(ui_Image2, LV_OBJ_FLAG_ADV_HITTEST);
+    lv_obj_clear_flag(ui_Image2, LV_OBJ_FLAG_SCROLLABLE);
+
+    // ui_Image3
+
+    ui_Image3 = lv_img_create(ui_Screen1);
+    lv_img_set_src(ui_Image3, &ui_img_rsz_tpmsicon_png);
+
+    lv_obj_set_width(ui_Image3, LV_SIZE_CONTENT);
+    lv_obj_set_height(ui_Image3, LV_SIZE_CONTENT);
+
+    lv_obj_set_x(ui_Image3, -70);
+    lv_obj_set_y(ui_Image3, 21);
+
+    lv_obj_set_align(ui_Image3, LV_ALIGN_CENTER);
+
+    lv_obj_add_flag(ui_Image3, LV_OBJ_FLAG_ADV_HITTEST);
+    lv_obj_clear_flag(ui_Image3, LV_OBJ_FLAG_SCROLLABLE);
+
+    // ui_Image4
+
+    ui_Image4 = lv_img_create(ui_Screen1);
+    lv_img_set_src(ui_Image4, &ui_img_rsz_tpmsicon_png);
+
+    lv_obj_set_width(ui_Image4, LV_SIZE_CONTENT);
+    lv_obj_set_height(ui_Image4, LV_SIZE_CONTENT);
+
+    lv_obj_set_x(ui_Image4, -70);
+    lv_obj_set_y(ui_Image4, 102);
+
+    lv_obj_set_align(ui_Image4, LV_ALIGN_CENTER);
+
+    lv_obj_add_flag(ui_Image4, LV_OBJ_FLAG_ADV_HITTEST);
+    lv_obj_clear_flag(ui_Image4, LV_OBJ_FLAG_SCROLLABLE);
+
+    // ui_Image5
+
+    ui_Image5 = lv_img_create(ui_Screen1);
+    lv_img_set_src(ui_Image5, &ui_img_rsz_tpmsicon_png);
+
+    lv_obj_set_width(ui_Image5, LV_SIZE_CONTENT);
+    lv_obj_set_height(ui_Image5, LV_SIZE_CONTENT);
+
+    lv_obj_set_x(ui_Image5, 70);
+    lv_obj_set_y(ui_Image5, 102);
+
+    lv_obj_set_align(ui_Image5, LV_ALIGN_CENTER);
+
+    lv_obj_add_flag(ui_Image5, LV_OBJ_FLAG_ADV_HITTEST);
+    lv_obj_clear_flag(ui_Image5, LV_OBJ_FLAG_SCROLLABLE);
+
+    // ui_Label5
+
+    ui_Label5 = lv_label_create(ui_Screen1);
+
+    lv_obj_set_width(ui_Label5, LV_SIZE_CONTENT);
+    lv_obj_set_height(ui_Label5, LV_SIZE_CONTENT);
+
+    lv_obj_set_x(ui_Label5, -70);
+    lv_obj_set_y(ui_Label5, 74);
+
+    lv_obj_set_align(ui_Label5, LV_ALIGN_CENTER);
+
+    lv_label_set_text(ui_Label5, "TPMS5");
+
+    // ui_Label6
+
+    ui_Label6 = lv_label_create(ui_Screen1);
+
+    lv_obj_set_width(ui_Label6, LV_SIZE_CONTENT);
+    lv_obj_set_height(ui_Label6, LV_SIZE_CONTENT);
+
+    lv_obj_set_x(ui_Label6, 70);
+    lv_obj_set_y(ui_Label6, 74);
+
+    lv_obj_set_align(ui_Label6, LV_ALIGN_CENTER);
+
+    lv_label_set_text(ui_Label6, "TPMS6");
 
 }
 
--- examples/lv_demos/main/ui.h
+++ examples/lv_demos/main/ui.h
@@ -1,10 +1,10 @@
 // SquareLine LVGL GENERATED FILE
 // EDITOR VERSION: SquareLine Studio 1.0.5
 // LVGL VERSION: 8.2
-// PROJECT: SquareLine_Project2
+// PROJECT: SquareLine_Project
 
-#ifndef _SQUARELINE_PROJECT2_UI_H
-#define _SQUARELINE_PROJECT2_UI_H
+#ifndef _SQUARELINE_PROJECT_UI_H
+#define _SQUARELINE_PROJECT_UI_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,9 +20,22 @@ extern lv_obj_t * ui_Screen1;
 extern lv_obj_t * ui_Label1;
 extern lv_obj_t * ui_Label2;
 extern lv_obj_t * ui_trailer;
+extern lv_obj_t * ui_Image12;
+extern lv_obj_t * ui_Image13;
+extern lv_obj_t * ui_Image1;
+extern lv_obj_t * ui_Label3;
+extern lv_obj_t * ui_Label4;
+extern lv_obj_t * ui_Image2;
+extern lv_obj_t * ui_Image3;
+extern lv_obj_t * ui_Image4;
+extern lv_obj_t * ui_Image5;
+extern lv_obj_t * ui_Label5;
+extern lv_obj_t * ui_Label6;
 
 
 LV_IMG_DECLARE(ui_img_rsz_picture4_png);    // assets\rsz_picture4.png
+LV_IMG_DECLARE(ui_img_rsz_dexter_big_png);    // assets\rsz_dexter_big.png
+LV_IMG_DECLARE(ui_img_rsz_tpmsicon_png);    // assets\rsz_tpmsicon.png
 
 
 
